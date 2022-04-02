d72a741a4d096b3cc78ffa9abf5df717053e35b1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-02 23:40:52 +0430

    routs added to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch as Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './Layout/layout';
+import { mainRoutes } from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          {mainRoutes.map((route) => {
+            return (
+              <Route
+                component={route.component}
+                path={route.path}
+                exact={route.isExact}
+                key={route.id}
+              />
+            );
+          })}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
