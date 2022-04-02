83ae197df54512bc5899c886011ffc29f09a8c5d
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-02 04:45:53 +0430

    routs.js updated

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,41 @@
+import AboutUsPage from './pages/about-us/about-us';
+import HomePage from './pages/home/home';
+import NotFoundPage from './pages/not-found/not-found';
+import ProfilePage from './pages/profile/profile';
 
-
-const routes = [
-  { isExact: true, component: '</>', path: '/', title: 'Home' },
-  { isExact: false, component: '</>', path: '/About-us/', title: 'About us' },
-  { isExact: false, component: '</>', path: '/profile/', title: 'Profile' },
+const mainRoutes = [
   {
     isExact: false,
-    component: '</>',
-    path: '/contactUs/',
-    title: 'Contact us',
+    component: AboutUsPage,
+    path: '/About-us/',
+    title: 'About us',
+    hasLink: true,
+    id: 0,
   },
-  { isExact: false, component: '</>', path: '/*', title: 'Not found' },
+  {
+    isExact: false,
+    component: ProfilePage,
+    path: '/profile/',
+    title: 'Profile',
+    hasLink: true,
+    id: 1,
+  },
+  {
+    isExact: true,
+    component: HomePage,
+    path: '/',
+    title: 'Home',
+    hasLink: true,
+    id: 2,
+  },
+  {
+    isExact: false,
+    component: NotFoundPage,
+    path: '/*',
+    title: 'Not found',
+    hasLink: false,
+    id: 3,
+  },
 ];
 
-export default routes;
+export default mainRoutes;
