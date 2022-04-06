617afb07f7264ecd98ca9dcc8b97456de19ff198
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-07 00:47:19 +0430

    practic with query string in PostPage

--- src/pages/post/postPage.jsx
+++ src/pages/post/postPage.jsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import { Link, useLocation } from 'react-router-dom';
 import styles from './postPage.module.scss';
+// import queryString from 'query-string';
+import React from 'react';
 
-const PostPage = ({ match }) => {
-  const paramId = match.params.id;
+// function useQuery() {
+//   const { search } = useLocation();
+
+//   return React.useMemo(() => new URLSearchParams(search), [search]);
+// }
+
+const PostPage = ({ match, location }) => {
+    const paramId = match.params.id;
+  //   const query = queryString.parse(location.search);
+
+//   const query = useQuery();
+
+//   console.log(query.get('location'));
   return (
     <section>
       <h1>{`post number ${paramId}`}</h1>
@@ -14,8 +28,12 @@ const PostPage = ({ match }) => {
         voluptate obcaecati!
       </p>
       <div className={styles.forwardAndBackwardBtnGroup}>
-        <Link to={`/blog/${+paramId - 1}`}>&lt; &lt;</Link>
-        <Link to={`/blog/${+paramId + 1}`}>&gt; &gt;</Link>
+        <Link key="next" to={`/blog/${+paramId - 1}`}>
+          &lt; &lt;
+        </Link>
+        <Link key="prev" to={`/blog/${+paramId + 1}`}>
+          &gt; &gt;
+        </Link>
       </div>
     </section>
   );
